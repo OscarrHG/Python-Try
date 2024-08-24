0a4237dc5d14576737a172e603b070019f79c96a
OscarrHG <hgoscar3@gmail.com>
2024-08-23 22:44:47 -0500

    Avance Operators

---
+++ Basics/00_hello.py
@@ -0,0 +1,19 @@
+# Holiwis
+# Esto es un comentario
+# Nuestro hola mundo en python
+
+"""
+Este es un comentario en bloque
+para poder escribir varias lineas de comentario
+colocando 3 comillas dobles
+"""
+
+print("Hola Python")
+print('Hola Python')
+
+# Consultar el tipo de dato
+print(type("hello")) # Tipo 'str'
+print(type(21)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(True)) # Tipo 'bool'
+print(type(1 + 2j)) # Tipo 'complex'
---
+++ Basics/02_operators.py
@@ -0,0 +1,16 @@
+"""
+Operators
+"""
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 /1 // 4)
+print(15 // 4) #Hacer division y el resultado aproximarla a un numero entero
+
+print("Hola " + "Python " + "WSP")
+print("Hola! " + str(5))
+print("Hola " * 5)
